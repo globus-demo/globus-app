cd72fcd21cf4ae15aa46d1c7c2fa0a52b8a06fae
Solyankin Ilia <evil.enot.00@gmail.com>
2021-04-06 22:41:43 +0300

    Add fragments (not working)

--- app/src/main/java/com/technopolis_education/globusapp/MainActivity.kt
+++ app/src/main/java/com/technopolis_education/globusapp/MainActivity.kt
@@ -3,7 +3,10 @@ package com.technopolis_education.globusapp
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,21 +14,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val bottomView: BottomNavigationView = findViewById(R.id.bottom_nav_bar)
+        val navController = findNavController(R.id.main_fragment)
         bottomView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.feed -> {
-                    startActivity(Intent(this, FeedActivity::class.java))
+                    navController.navigate(R.id.action_main_fragment_to_feed_fragment)
                     true
                 }
                 R.id.messages -> {
-                    startActivity(Intent(this, MainMessenger::class.java))
+                    navController.navigate(R.id.action_feed_fragment_to_message_fragment)
                     true
                 }
                 R.id.globe -> {
+                    navController.navigate(R.id.action_feed_fragment_to_message_fragment)
                     true
                 }
                 R.id.profile -> {
-                    startActivity(Intent(this, ProfileActivity::class.java))
+                    navController.navigate(R.id.action_feed_fragment_to_profile_fragment)
                     true
                 }
                 else -> false
--- app/src/main/java/com/technopolis_education/globusapp/ProfileActivity.kt
+++ app/src/main/java/com/technopolis_education/globusapp/ProfileActivity.kt
@@ -2,35 +2,22 @@ package com.technopolis_education.globusapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class ProfileActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.profile_page)
-        setTitle(R.string.profile_page)
+class ProfileActivity : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.profile_page, container, false)
 
-        val bottomView: BottomNavigationView = findViewById(R.id.bottom_nav_bar)
-        bottomView.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.feed -> {
-                    startActivity(Intent(this, FeedActivity::class.java))
-                    true
-                }
-                R.id.messages -> {
-                    startActivity(Intent(this, MainMessenger::class.java))
-                    true
-                }
-                R.id.globe -> {
-                    true
-                }
-                R.id.profile -> {
-                    startActivity(Intent(this, ProfileActivity::class.java))
-                    true
-                }
-                else -> false
-            }
-        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 }
